3af27067702881e6375a9cb2a7f108e5109aafa3
Montserrat Ximena Hernández Gallegos <gallegos230804@gmail.com>
2023-10-10 20:27:27 -0600

    1era version

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace curbadebezier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.P1X = new System.Windows.Forms.TextBox();
+            this.grafica = new System.Windows.Forms.Panel();
+            this.P1Y = new System.Windows.Forms.TextBox();
+            this.P2X = new System.Windows.Forms.TextBox();
+            this.P2Y = new System.Windows.Forms.TextBox();
+            this.P3X = new System.Windows.Forms.TextBox();
+            this.P3Y = new System.Windows.Forms.TextBox();
+            this.P4X = new System.Windows.Forms.TextBox();
+            this.P4Y = new System.Windows.Forms.TextBox();
+            this.actualizar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.actualizar);
+            this.panel1.Controls.Add(this.P4Y);
+            this.panel1.Controls.Add(this.P4X);
+            this.panel1.Controls.Add(this.P3Y);
+            this.panel1.Controls.Add(this.P3X);
+            this.panel1.Controls.Add(this.P2Y);
+            this.panel1.Controls.Add(this.P2X);
+            this.panel1.Controls.Add(this.P1Y);
+            this.panel1.Controls.Add(this.P1X);
+            this.panel1.Controls.Add(this.grafica);
+            this.panel1.Location = new System.Drawing.Point(3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(793, 442);
+            this.panel1.TabIndex = 0;
+            // 
+            // P1X
+            // 
+            this.P1X.Location = new System.Drawing.Point(685, 39);
+            this.P1X.Name = "P1X";
+            this.P1X.Size = new System.Drawing.Size(100, 22);
+            this.P1X.TabIndex = 1;
+            this.P1X.TextChanged += new System.EventHandler(this.P1X_TextChanged);
+            // 
+            // grafica
+            // 
+            this.grafica.Location = new System.Drawing.Point(4, 4);
+            this.grafica.Name = "grafica";
+            this.grafica.Size = new System.Drawing.Size(617, 438);
+            this.grafica.TabIndex = 0;
+            this.grafica.Paint += new System.Windows.Forms.PaintEventHandler(this.grafica_Paint);
+            // 
+            // P1Y
+            // 
+            this.P1Y.Location = new System.Drawing.Point(685, 68);
+            this.P1Y.Name = "P1Y";
+            this.P1Y.Size = new System.Drawing.Size(100, 22);
+            this.P1Y.TabIndex = 2;
+            this.P1Y.TextChanged += new System.EventHandler(this.P1Y_TextChanged);
+            // 
+            // P2X
+            // 
+            this.P2X.Location = new System.Drawing.Point(685, 128);
+            this.P2X.Name = "P2X";
+            this.P2X.Size = new System.Drawing.Size(100, 22);
+            this.P2X.TabIndex = 3;
+            this.P2X.TextChanged += new System.EventHandler(this.P2X_TextChanged);
+            // 
+            // P2Y
+            // 
+            this.P2Y.Location = new System.Drawing.Point(685, 156);
+            this.P2Y.Name = "P2Y";
+            this.P2Y.Size = new System.Drawing.Size(100, 22);
+            this.P2Y.TabIndex = 4;
+            this.P2Y.TextChanged += new System.EventHandler(this.P2Y_TextChanged);
+            // 
+            // P3X
+            // 
+            this.P3X.Location = new System.Drawing.Point(685, 237);
+            this.P3X.Name = "P3X";
+            this.P3X.Size = new System.Drawing.Size(100, 22);
+            this.P3X.TabIndex = 5;
+            this.P3X.TextChanged += new System.EventHandler(this.P3X_TextChanged);
+            // 
+            // P3Y
+            // 
+            this.P3Y.Location = new System.Drawing.Point(685, 265);
+            this.P3Y.Name = "P3Y";
+            this.P3Y.Size = new System.Drawing.Size(100, 22);
+            this.P3Y.TabIndex = 6;
+            this.P3Y.TextChanged += new System.EventHandler(this.P3Y_TextChanged);
+            // 
+            // P4X
+            // 
+            this.P4X.Location = new System.Drawing.Point(685, 336);
+            this.P4X.Name = "P4X";
+            this.P4X.Size = new System.Drawing.Size(100, 22);
+            this.P4X.TabIndex = 7;
+            this.P4X.TextChanged += new System.EventHandler(this.P4X_TextChanged);
+            // 
+            // P4Y
+            // 
+            this.P4Y.Location = new System.Drawing.Point(685, 364);
+            this.P4Y.Name = "P4Y";
+            this.P4Y.Size = new System.Drawing.Size(100, 22);
+            this.P4Y.TabIndex = 8;
+            this.P4Y.TextChanged += new System.EventHandler(this.P4Y_TextChanged);
+            // 
+            // actualizar
+            // 
+            this.actualizar.Location = new System.Drawing.Point(672, 413);
+            this.actualizar.Name = "actualizar";
+            this.actualizar.Size = new System.Drawing.Size(75, 23);
+            this.actualizar.TabIndex = 9;
+            this.actualizar.Text = "Actualizar";
+            this.actualizar.UseVisualStyleBackColor = true;
+            this.actualizar.Click += new System.EventHandler(this.actualizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox P1X;
+        private System.Windows.Forms.Panel grafica;
+        private System.Windows.Forms.TextBox P4Y;
+        private System.Windows.Forms.TextBox P4X;
+        private System.Windows.Forms.TextBox P3Y;
+        private System.Windows.Forms.TextBox P3X;
+        private System.Windows.Forms.TextBox P2Y;
+        private System.Windows.Forms.TextBox P2X;
+        private System.Windows.Forms.TextBox P1Y;
+        private System.Windows.Forms.Button actualizar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace curbadebezier
+{
+    public partial class Form1 : Form
+    {
+        private List<PointF> puntos = new List<PointF>(); // Lista que almacena los puntos de control de la curva de Bezier
+        private Point puntoArrastrado = Point.Empty; // Punto que se está arrastrando
+        private int radioPunto = 5; // Radio de los puntos de control
+        private int puntoSeleccionado = -1; // Variable para llevar un registro del punto seleccionado
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            puntos.Add(new PointF(50, 150));
+            puntos.Add(new PointF(150, 50));
+            puntos.Add(new PointF(250, 150));
+            puntos.Add(new PointF(350, 250));
+
+            // Crear etiquetas para representar los puntos de control
+            for (int i = 0; i < puntos.Count; i++)
+            {
+                var label = new Label
+                {
+                    Location = new Point((int)puntos[i].X - radioPunto, (int)puntos[i].Y - radioPunto),
+                    Size = new Size(2 * radioPunto, 2 * radioPunto),
+                    BackColor = Color.Red,
+                    Cursor = Cursors.Hand,
+                    Tag = i, // Utiliza el atributo 'Tag' para identificar el punto de control
+                };
+                label.MouseDown += Punto_MouseDown; // Asigna el evento MouseDown para permitir arrastrar el punto
+                label.MouseMove += Punto_MouseMove; // Asigna el evento MouseMove para actualizar la posición del punto
+                label.MouseUp += Punto_MouseUp; // Asigna el evento MouseUp para finalizar el arrastre
+                Controls.Add(label);
+            }
+        }
+
+        private void Punto_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Registra el punto seleccionado para el arrastre
+            puntoSeleccionado = (int)((Label)sender).Tag;
+        }
+
+        private void Punto_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (puntoSeleccionado != -1)
+            {
+                int puntoIndex = puntoSeleccionado;
+                int x = e.X + ((Label)sender).Left + radioPunto;
+                int y = e.Y + ((Label)sender).Top + radioPunto;
+
+                // Actualiza la posición del punto de control
+                puntos[puntoIndex] = new PointF(x, y);
+
+                ((Label)sender).Left = x - radioPunto;
+                ((Label)sender).Top = y - radioPunto;
+
+                // Actualiza los valores de los TextBox con las nuevas coordenadas
+                ActualizarTextBoxConPunto(puntoIndex);
+
+                // Vuelve a dibujar la gráfica
+                grafica.Refresh();
+            }
+        }
+
+        private void Punto_MouseUp(object sender, MouseEventArgs e)
+        {
+            puntoSeleccionado = -1; // Restablece el punto seleccionado;
+        }
+
+        private void ActualizarTextBoxConPunto(int puntoIndex) {
+            // Actualiza los valores de los TextBox con las coordenadas del punto de control
+            TextBox textBoxX = (TextBox)Controls.Find("P" + (puntoIndex + 1) + "X", true)[0];
+            TextBox textBoxY = (TextBox)Controls.Find("P" + (puntoIndex + 1) + "Y", true)[0];
+
+            textBoxX.Text = puntos[puntoIndex].X.ToString();
+            textBoxY.Text = puntos[puntoIndex].Y.ToString();
+        }
+
+        //COORDADA DEL PUNTO 1 EJE X
+        private void P1X_TextChanged(object sender, EventArgs e)
+        {
+            ActualizarPuntoDesdeTextBox(0, P1X, P1Y);
+        }
+
+        //COORDADA DEL PUNTO 1 EJE Y
+        private void P1Y_TextChanged(object sender, EventArgs e)
+        {
+            ActualizarPuntoDesdeTextBox(0, P1X, P1Y);
+        }
+
+        //COORDADA DEL PUNTO 2 EJE X
+        private void P2X_TextChanged(object sender, EventArgs e)
+        {
+            ActualizarPuntoDesdeTextBox(1, P2X, P2Y);
+        }
+
+        //COORDADA DEL PUNTO 2 EJE Y
+        private void P2Y_TextChanged(object sender, EventArgs e)
+        {
+            ActualizarPuntoDesdeTextBox(1, P2X, P2Y);
+        }
+
+        //COORDADA DEL PUNTO 3 EJE X
+        private void P3X_TextChanged(object sender, EventArgs e)
+        {
+            ActualizarPuntoDesdeTextBox(2, P3X, P3Y);
+        }
+
+        //COORDADA DEL PUNTO 3 EJE Y
+        private void P3Y_TextChanged(object sender, EventArgs e)
+        {
+            ActualizarPuntoDesdeTextBox(2, P3X, P3Y);
+        }
+
+        //COORDADA DEL PUNTO 4 EJE X
+        private void P4X_TextChanged(object sender, EventArgs e)
+        {
+            ActualizarPuntoDesdeTextBox(3, P4X, P4Y);
+        }
+
+        //COORDADA DEL PUNTO 4 EJE Y
+        private void P4Y_TextChanged(object sender, EventArgs e)
+        {
+            ActualizarPuntoDesdeTextBox(3, P4X, P4Y);
+        }
+
+
+        private void ActualizarPuntoDesdeTextBox(int puntoIndex, TextBox textBoxX, TextBox textBoxY)
+        {
+            float x, y;
+            if (float.TryParse(textBoxX.Text, out x) && float.TryParse(textBoxY.Text, out y))
+            {
+                puntos[puntoIndex] = new PointF(x, y);
+                ActualizarPosicionPuntoControl(puntoIndex);
+
+                // Vuelve a dibujar la gráfica
+                grafica.Refresh();
+            }
+        }
+
+        //BOTON QUE ACTUALIZA LA GRAFICA CON LOS COORDENADAS LLENADAS EN LOS TEXTS BOXS
+        private void actualizar_Click(object sender, EventArgs e)
+        {
+            // Actualiza los puntos de control con los valores de los TextBox
+            for (int i = 0; i < puntos.Count; i++)
+            {
+                float x, y;
+                if (float.TryParse(((TextBox)Controls.Find("P" + (i + 1) + "X", true)[0]).Text, out x) &&
+                    float.TryParse(((TextBox)Controls.Find("P" + (i + 1) + "Y", true)[0]).Text, out y))
+                {
+                    puntos[i] = new PointF(x, y);
+                    ActualizarPosicionPuntoControl(i);
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, ingrese valores válidos para las coordenadas de los puntos de control.", "Error de entrada");
+                    return; // Detenemos la actualización si hay un error en la entrada
+                }
+            }
+
+            // Vuelve a dibujar la gráfica
+            grafica.Refresh();
+        }
+
+        private void ActualizarPosicionPuntoControl(int puntoIndex)
+        {
+            // Actualizar la posición de la etiqueta que representa el punto de control
+            Controls[puntoIndex].Location = new Point((int)puntos[puntoIndex].X - radioPunto, (int)puntos[puntoIndex].Y - radioPunto);
+        }
+
+        //EN ESTE OBJETO VA LA GRAFICA
+        private void grafica_Paint(object sender, PaintEventArgs e)
+        {
+            if (puntos.Count < 4) return;
+
+            var bezierPoints = CalcularCurvaDeBezier(puntos[0], puntos[1], puntos[2], puntos[3]);
+            var graphics = e.Graphics;
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            // Dibuja la curva de Bezier
+            using (var pen = new Pen(Color.Blue, 2))
+            {
+                graphics.DrawLines(pen, bezierPoints.ToArray());
+            }
+
+            // Dibuja los puntos de control
+            foreach (var punto in puntos)
+            {
+                graphics.FillEllipse(Brushes.Red, punto.X - radioPunto, punto.Y - radioPunto, 2 * radioPunto, 2 * radioPunto);
+            }
+        }
+
+        private List<PointF> CalcularCurvaDeBezier(PointF p0, PointF p1, PointF p2, PointF p3)
+        {
+            // Crea una lista para almacenar los puntos que formarán la curva de Bezier
+            List<PointF> curvePoints = new List<PointF>();
+            // El bucle itera desde t = 0 hasta t = 1 con incrementos de 0.01. Esto permite calcular varios puntos en la curva.
+            for (float t = 0; t <= 1; t += 0.01f)
+            {
+                // Para cada valor de t, se calcula una nueva posición (x, y) en la curva de Bezier.
+
+                // La fórmula para calcular x utiliza la ecuación de Bezier para coordenadas x:
+                float x = (float)((1 - t) * (1 - t) * (1 - t) * p0.X +
+                                3 * (1 - t) * (1 - t) * t * p1.X +
+                                3 * (1 - t) * t * t * p2.X +
+                                t * t * t * p3.X);
+
+                // La fórmula para calcular y utiliza la ecuación de Bezier para coordenadas y:
+                float y = (float)((1 - t) * (1 - t) * (1 - t) * p0.Y +
+                                3 * (1 - t) * (1 - t) * t * p1.Y +
+                                3 * (1 - t) * t * t * p2.Y +
+                                t * t * t * p3.Y);
+
+                // Se agrega el nuevo punto (x, y) a la lista de puntos en la curva
+                curvePoints.Add(new PointF(x, y));
+            }
+
+            // Devuelve la lista de puntos que forman la curva de Bezier
+            return curvePoints;
+        }
+    }
+}
